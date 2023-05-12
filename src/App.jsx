6419f6c58d82a3d75fdb4cfc08347967c5f45445
iak1p <70378272+iak1p@users.noreply.github.com>
2023-05-12 22:19:56 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import "./App.css";
 import LeftSidebar from "./components/LeftSidebar/LeftSidebar";
 import RightSidebar from "./components/RightSidebar/RightSidebar";
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    fetch("../users.json", {
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((users) => {
-        dispatch({
-          type: "SET_USERS",
-          payload: users,
-        });
-      });
-  }, []);
-
   return (
     <>
       <div className="app">
